02c535a5e950596c1815103f27909527f2250df9
Han Ji Hye <37392113+jh1023@users.noreply.github.com>
2023-10-19 08:56:06 +0900

    [level unrated] Title: 문자열 돌리기, Time: 1.46 ms, Memory: 4.23 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define LEN_INPUT 11
+
+int main(void) {
+    char s1[LEN_INPUT];
+    scanf("%s", s1);
+    for (int i=0; i<strlen(s1); i++){
+        printf("%c\n", s1[i]);
+    }
+   
+    return 0;
+}
