a05df0c7b65e5c39cb4e89553cebaed39db86659
Han Ji Hye <37392113+jh1023@users.noreply.github.com>
2023-11-01 08:48:19 +0900

    [level unrated] Title: 공배수, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181936. 공배수/공배수.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int number, int n, int m) {
+    int answer = 0;
+    
+    answer = number % n == 0 && number % m == 0 ? 1:0; 
+    return answer;
+}
