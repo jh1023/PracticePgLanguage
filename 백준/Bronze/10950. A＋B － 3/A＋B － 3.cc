1b93bce7db2b007072334405058a8c60bfe60779
Han Ji Hye <37392113+jh1023@users.noreply.github.com>
2023-11-06 20:38:40 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+	int t;
+	scanf("%d", &t);
+
+	int a, b;
+	for (int i = 0; i < t; ++i) {
+		scanf("%d %d", &a, &b);
+		printf("%d\n", a + b);
+	}
+
+	return 0;
+}
