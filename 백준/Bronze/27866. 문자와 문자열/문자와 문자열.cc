324c8d192b371813e244319f2d4bc13980c71fb0
Han Ji Hye <37392113+jh1023@users.noreply.github.com>
2023-11-30 09:02:20 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std; 
+int main()
+{
+    string name; 
+    cin >> name;
+	int n;
+	cin >> n;
+	
+    cout <<  name[n-1] << endl;
+    return 0;
+}
