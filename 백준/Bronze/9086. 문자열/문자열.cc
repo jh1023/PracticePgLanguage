af52cbfb660c583a420fc9aea87bc165334c89f2
Han Ji Hye <37392113+jh1023@users.noreply.github.com>
2023-12-02 21:09:15 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+ 
+int main() {
+    int t;
+    string str;
+    cin >> t;
+    
+    for(int i = 0; i < t; i++) {
+        cin >> str;
+        int size = str.length();
+        cout << str[0] << str[size-1] << "\n";
+    }
+    
+    return 0;
+}
