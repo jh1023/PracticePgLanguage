f27a6192546695a37bcf74e9ae55f8c4e045e34e
Han Ji Hye <37392113+jh1023@users.noreply.github.com>
2023-12-22 07:47:22 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() { 
+	int N;                   
+	string word;                       
+	int count = 0;                      
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> word;
+		bool alphabet[26] = { false, };        
+		alphabet[(int)(word[0]) - 97] = true; 
+
+		for (int i = 1; i < word.size(); i++) {
+			if (word[i] == word[i - 1]) {
+				continue;
+			}
+			else if(word[i] != word[i - 1] 
+            		&& alphabet[(int)(word[i]) - 97] == true){
+				count++;	
+				break;
+			}
+			else {
+				alphabet[(int)(word[i]) - 97] = true;
+			}
+		}
+	}
+
+	cout << N - count;
+
+	return 0;
+}
