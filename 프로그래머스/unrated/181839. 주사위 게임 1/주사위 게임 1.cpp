24ac89e8fd2137f2ef6e0d8a33ef909bd14a00b7
Han Ji Hye <37392113+jh1023@users.noreply.github.com>
2024-01-06 15:20:58 +0900

    [level unrated] Title: 주사위 게임 1, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int answer = 0;
+    if(a % 2 != 0 && b % 2 != 0)
+        answer = pow(a, 2) + pow(b, 2);        
+    else if(a % 2 != 0 || b % 2 != 0) 
+        answer = 2 * (a + b);
+    else if(a % 2 == 0 && b % 2 == 0) 
+        answer = abs(a - b);
+    
+    return answer;
+}
