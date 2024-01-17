32971ced264c4d8179d5566768afc5a164726ddd
Han Ji Hye <37392113+jh1023@users.noreply.github.com>
2024-01-18 08:52:46 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

---
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int solution(string num_str) {
+    int answer = 0;
+    for(int i=0; i< num_str.size(); i++) {
+        answer += num_str[i] - '0';        
+    }
+
+    return answer;
+}
