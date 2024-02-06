0f6f53c17f6da8ad1cf5b1979ff9d0d24a68d9f3
Han Ji Hye <37392113+jh1023@users.noreply.github.com>
2024-02-07 08:53:10 +0900

    [level 0] Title: rny_string, Time: 0.02 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <regex>
+
+using namespace std;
+
+string solution(string rny_string) {
+    string answer = regex_replace(rny_string, regex("m"), "rn");
+    return answer; 
+}
