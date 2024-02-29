05b3938604f9c674945835e27eacccb7d965e4be
Han Ji Hye <37392113+jh1023@users.noreply.github.com>
2024-03-01 08:57:55 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = -1;
+    for (int i = 0; i < num_list.size(); i++){
+        if(num_list[i] < 0)
+            return i;
+    }
+    return answer;
+}
