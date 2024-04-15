607c6e81e77c70925b44d3864fb4e50fb8f2c755
Han Ji Hye <37392113+jh1023@users.noreply.github.com>
2024-04-16 08:57:28 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solution(string str1, string str2) {
+    int answer = 0;
+    answer = (str1.find(str2) != string::npos) ? 1:2;
+    
+    return answer;
+}
