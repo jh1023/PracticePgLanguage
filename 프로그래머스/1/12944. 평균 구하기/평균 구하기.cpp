ee05309d3f03b9fcaffd36cca4c2edb18afc8253
Han Ji Hye <37392113+jh1023@users.noreply.github.com>
2024-06-13 08:59:09 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    double sum = 0;
+    for(int i = 0; i<arr.size(); i++) {
+        sum += arr[i];
+    }
+    double answer = sum/arr.size();
+    return answer;
+}
