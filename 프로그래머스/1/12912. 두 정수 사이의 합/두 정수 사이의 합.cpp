ee6ec801bf76147edd495dba1a17a76324ea8ec8
Han Ji Hye <37392113+jh1023@users.noreply.github.com>
2024-06-25 08:55:47 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.cpp
@@ -0,0 +1,18 @@
+
+using namespace std;
+
+long long solution(int a, int b) {
+    long long answer = 0;
+    
+    if ( a == b ){
+        return a;
+    } else if (a < b) {
+        for (int i = a; i <= b; i++)
+            answer += i;
+    } else {
+        for (int i = b; i <= a; i++)
+            answer += i;
+    }
+        
+    return answer;
+}
